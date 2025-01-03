a28bbef942d63c8c7e62cf2cd9e68f80ec0210c4
Neto Costa <neto1809costa@gmail.com>
2025-01-02 23:09:42 -0300

    unit 4: objects and classes
    Signed-off-by: Neto Costa <neto1809costa@gmail.com>

--- sketches/sketch-02.js
+++ sketches/sketch-02.js
@@ -1,4 +1,7 @@
 const canvasSketch = require('canvas-sketch');
+const math = require('canvas-sketch-util/math');
+const random = require('canvas-sketch-util/random');
+
 
 const settings = {
   dimensions: [ 1080, 1080 ]
@@ -6,6 +9,10 @@ const settings = {
 
 const degToRad= (degrees) => {
 	return degrees / 180 * Math.PI;
+}
+
+const randomRange = (min, max) => {
+  return Math.random() * (max - min) + min;
 }
 
 const sketch = () => {
@@ -21,11 +28,11 @@ const sketch = () => {
     const h = height * 0.1;
     let x,y;
 
-    const num = 12;
+    const num = 40;
     const radius = width * 0.3;
 
     for(let i = 0; i < num; i++) {
-	    const slice = degToRad(360/num);
+	    const slice = math.degToRad(360/num);
 	    const angle = slice * i;
 
 	    x = cx + radius * Math.sin(angle);   
@@ -34,14 +41,24 @@ const sketch = () => {
 	    context.save();
 	    context.translate(x, y);
 	    context.rotate(-angle);
+      context.scale(random.range(0.1, 2), random.range(0.2, 0.5));
 
 	    context.beginPath();
-	    context.rect(-w * 0.5, -h * 0.5, w, h);
+	    context.rect(-w * 0.5, random.range(0, -h * 0.5), w, h);
 	    context.fill();
 	    context.restore();
+
+      context.save();
+      context.translate(cx, cy);
+      context.rotate(-angle);
+
+      context.lineWidth = random.range(5, 20);
+      context.beginPath();
+      context.arc(0, 0, radius * random.range(0.7, 1.3) , slice * random.range(1, -8), slice * random.range(1, 5));
+      context.stroke();
+
+      context.restore();
     }
-
-
   };
 };
 
